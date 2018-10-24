5e5d745cd305210c563144b343d09f269e0781ac
justinbaskaran <justinbaskaran5@gmail.com>
2018-10-24 14:52:44 -0400

    Lab08update

--- lab08/Lab8.sql
+++ lab08/Lab8.sql
@@ -11,14 +11,11 @@
 -- SELECT * FROM player WHERE RIGHT(emailAddress,9) = 'gmail.edu';
 
 ------------------8.2---------------------------
---SELECT game.time FROM player, game,playergame WHERE player.name='The King' AND player.ID = playergame.playerID AND playergame.gameID = game.ID;
+--SELECT * FROM player,playergame WHERE player.ID = playergame.PlayerID AND player.name='The King';
 
 -- SELECT player.name FROM playergame, player,game WHERE game.time='2006-06-28 13:20:00' AND game.ID = playergame.gameID AND playergame.playerID = player.ID AND playergame.score != 0.00;
 
--- c. It does a cartesian product with itself,
---  on ID's greater then the 
--- first value. For example. 1 does a cartesian product with 2,3,...
--- 2 does a cartesian product 3,4...
+-- c. Makes sure Id of the first instance of player table is greater then the second instance of the player table, all whiel making sure their names are equal. 
 
 -- d. Perhaps you want to find the permutations of x players,
 -- for this you could usse a self join to find the different
